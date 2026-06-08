e112979777c9d77da0bf8c0d7b46c93d6f0b44f8
muhammad <pxtel.muhammad@gmail.com>
2026-06-08 15:27:28 +0530

    fix(alg-sort): rewrite insertion sort logic

--- Algorithms/Sorting/insertionSort.cpp
+++ Algorithms/Sorting/insertionSort.cpp
@@ -9,10 +9,9 @@ vector<int> insertionSort(vector<int> nums)
 {
     for (int i = 1; i < nums.size(); i++)
     {
-        for(int j=i;j>0 && nums[j]<nums[j-1]; j--)
-            swap(nums[j],nums[j-1]);
-        }
-
+        for (int j = i; j > 0 && nums[j] < nums[j - 1]; j--)
+            swap(nums[j], nums[j - 1]);
+    }
     return nums;
 }
 
